d2a0574066280612bc57ddd27fa2f2b25152b4a9
dgerard42 <daniella4422@gmail.com>
2020-02-04 15:56:31 -0800

    hailstone done

--- sa_04/src/Hailstone.java
+++ sa_04/src/Hailstone.java
@@ -1,15 +1,25 @@
 import java.util.Scanner;
 
-public class Hailstone extends {
-
+public class Hailstone {
     public int getUserInput(){
+        System.out.println("please enter a non-zero number");
         Scanner keyboard = new Scanner(System.in);
-        
+        int n = 0;
+        while (n == 0){
+            if (keyboard.hasNextInt()) {
+                n = keyboard.nextInt();
+            }
+            if (n == 0){
+                keyboard.nextLine();
+                System.out.println("invalid input, pls try again");
+            }
+        }
+        return n;
     }
 
     public static void main(String[] args) {
-        new Hailstone();
-        int n = getUserInput();
+        Hailstone sequence = new Hailstone();
+        int n = sequence.getUserInput();
         while (n != 1){
             System.out.println(n);
             if (n % 2 == 0)
